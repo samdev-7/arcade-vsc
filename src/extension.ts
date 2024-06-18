d417a48f2119705d7ca70cc48d86817b1e0bf053
Sam Liu <hello@samliu.dev>
2024-06-18 19:46:01 -0400

    Saving and validation of slack IDs

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,92 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fetch from 'node-fetch';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const idTest = (id: string): boolean => !!id && /^[A-Z0-9]{5,}$/.test(id);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "arcade-vsc" is now active!');
+const hc_endpoint = "https://hackhour.hackclub.com/api/clock/";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('arcade-vsc.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Arcade VSC!');
+async function checkID(id: string): Promise<boolean> | never {
+	const res = await fetch(hc_endpoint + id, {
+		method: 'GET',
+		headers: {
+			'User-Agent': 'Arcade VSC Extension'
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	switch (res.status) {
+		case 200:
+			return true;
+		case 404:
+			return false;
+		default:
+			throw new Error(`Unexpected status code of ${res.status}`);
+	}
+
+}
+
+async function saveID(context: vscode.ExtensionContext, id: string): Promise<void> {
+	await context.globalState.update('arcade-vsc-id', id);
+}
+
+async function getID(context: vscode.ExtensionContext): Promise<string | undefined> {
+	return context.globalState.get('arcade-vsc-id');
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('arcade-vsc is now active!');
+
+	if (context.globalState.get('arcade-vsc-id') === undefined) {
+		vscode.window.showInformationMessage('Arcade VSC is not set up yet. Run the "Arcade: Init" command to get started!');
+	}
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('arcade-vsc.init', async () => {
+			const prev_id = await getID(context);
+
+			let id = await vscode.window.showInputBox({
+				prompt: 'Enter your user ID from the Hack Club Slack',
+				placeHolder: !!prev_id ? 'This will overwrite your previous saved ID' : 'You can get this from #what-is-my-slack-id',
+				validateInput: (id: string) => idTest(id) ? '' : 'Please enter a valid Slack ID (not your username)'
+			});
+
+			if (id === undefined) {
+				vscode.window.showWarningMessage('Exited Arcade VSC setup as no ID was provided');
+				return;
+			};
+
+			try {
+				const res = await checkID(id);
+				if (res) {
+					await saveID(context, id);
+					vscode.window.showInformationMessage('ID has been validated and saved successfully!');
+				} else {
+					vscode.window.showErrorMessage('ID is invalid! Please check your ID and try again');
+					return;
+				}
+			} catch (e: unknown) {
+				if (e instanceof Error) {
+					vscode.window.showErrorMessage('Failed to check ID: ' + e.message);
+					return;
+				} else {
+					vscode.window.showErrorMessage('Failed to check ID: Unknown error');
+					return;
+				}
+			}
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('arcade-vsc.clear', async () => {
+			if (await getID(context) === undefined) {
+				vscode.window.showWarningMessage('No ID is saved');
+				return;
+			}
+			await context.globalState.update('arcade-vsc-id', undefined);
+			vscode.window.showInformationMessage('Cleared saved ID');
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
